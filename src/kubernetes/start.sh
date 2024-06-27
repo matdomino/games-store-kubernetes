8534863219f4303dcd1e36a99f9ab1bfe1a9614b
Mateusz Domino <mdomino60@gmail.com>
2024-06-27 02:52:34 +0200

    Added kubernetes

---
+++ src/kubernetes/start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+kubectl apply -f mongo-db-persistentvolume.yaml
+
+kubectl apply -f mongo-db-deployment.yaml
+kubectl apply -f express-server-deployment.yaml
+kubectl apply -f frontend-deployment.yaml
+
+kubectl apply -f mongo-db-service.yaml
+kubectl apply -f express-server-service.yaml
+kubectl apply -f frontend-service.yaml
+
+kubectl apply -f nginx-deployment.yaml
+kubectl apply -f nginx-service.yaml
---
+++ src/kubernetes/stop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+kubectl delete -f nginx-service.yaml
+kubectl delete -f nginx-deployment.yaml
+
+kubectl delete -f frontend-service.yaml
+kubectl delete -f express-server-service.yaml
+kubectl delete -f mongo-db-service.yaml
+
+kubectl delete -f frontend-deployment.yaml
+kubectl delete -f express-server-deployment.yaml
+kubectl delete -f mongo-db-deployment.yaml
